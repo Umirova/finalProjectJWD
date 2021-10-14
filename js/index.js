e7f528ad3188bee9900983e4ecce934d908b2711
Nazgul Umirova <nazgul@Nurbeks-MacBook-Pro.local>
2021-10-15 08:59:14 +1100

    edited delete button

--- js/index.js
+++ js/index.js
@@ -127,7 +127,7 @@ taskList.addEventListener("click", (event) => {
     taskManager.save();
     taskManager.render();
     //updates webpage after deleting task
-    window.location.href = window.location.href;
+    location.reload();
   }
   if (event.target.classList.contains("delete-svg")) {
     let parentTask =
